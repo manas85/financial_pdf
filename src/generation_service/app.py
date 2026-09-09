ccfa5817b389f61c8a3dfd828f327cf19d7b3cf5
Saumya pandey <saumyapandey@Saumyas-iMac.local>
2026-09-09 10:28:44 +0530

    Update PDF processing code

--- src/generation_service/app.py
+++ src/generation_service/app.py
@@ -13,7 +13,7 @@ sys.path.append(PROJECT_ROOT)
 # Load environment variables
 load_dotenv(dotenv_path=os.path.join(PROJECT_ROOT, '.env'))
 
-from src.embedding_service.embedder import GeminiEmbedder
+from src.embedding_service.embedder import LangchainEmbedder
 from src.embedding_service.qdrant_manager import QdrantManager
 from src.generation_service.gemini_rag_llm import GeminiRAG
 from src.generation_service.validation import QueryRequest, QueryResponse, UploadResponse
@@ -69,7 +69,7 @@ def startup_event():
     print("Initializing clients for generation service...")
     try:
         qdrant_manager = init_qdrant_manager(collection_name)
-        embedder = GeminiEmbedder(model_name="gemini-embedding-2")
+        embedder = LangchainEmbedder()
         rag_llm = GeminiRAG()
         print("All clients successfully initialized.")
     except Exception as e:
